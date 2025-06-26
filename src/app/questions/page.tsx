1d5a26dcbc853034e56a2929fb7a7421c3681ec4
pratham-soni <spratham304@gmail.com>
2025-06-26 18:53:06 +0530

    added question minimum-size-subarray-sum

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,21 +1,39 @@
-'use client';
+"use client";
 
 import FancyButton from "@/components/fancyButton";
-import {useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function QuestionsPage() {
   const router = useRouter();
   const handleQuestionNavigation = (link: string) => {
     router.push(link);
-  }
+  };
 
   return (
     <main className="min-h-screen bg-orange-100 px-4">
       <div className="flex flex-col justify-center items-center pt-10">
-        <h1 className="text-3xl font-bold mb-10 text-gray-800">Choose a algorithm</h1>
+        <h1 className="text-3xl font-bold mb-10 text-gray-800">
+          Choose a algorithm
+        </h1>
         <div className="flex flex-col gap-6">
-          <FancyButton text="Two sum" onClick={() => { handleQuestionNavigation("/questions/two-sum") }} />
-          <FancyButton text="Level order traversal in tree" onClick={() => { handleQuestionNavigation("/questions/level-traversal") }} />
+          <FancyButton
+            text="Two sum"
+            onClick={() => {
+              handleQuestionNavigation("/questions/two-sum");
+            }}
+          />
+          <FancyButton
+            text="Level order traversal in tree"
+            onClick={() => {
+              handleQuestionNavigation("/questions/level-traversal");
+            }}
+          />
+          <FancyButton
+            text="Minimum Size Subarray Sum"
+            onClick={() => {
+              handleQuestionNavigation("/questions/minimum-size-subarray-sum");
+            }}
+          />
         </div>
       </div>
     </main>
